ea915c981e9992c2a95b420226ff19b6aa53923f
axuata <axuata@gmail.com>
2025-05-01 20:38:15 +0900

    style: Adjusted header height

--- components/layouts/default/CHeader.vue
+++ components/layouts/default/CHeader.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <header class="font-en h-55px flex items-center justify-between">
+  <header class="font-en h-60px flex items-center justify-between">
     <div>
       <img src="/favicon.svg" alt="Axuata's icon" class="size-52px" />
     </div>
@@ -11,7 +11,7 @@
       <NuxtLink to="/" class="transition-all duration-100 no-underline text-gray-7 font-500 hover:tracking-wide">Home</NuxtLink>
       <NuxtLink to="/gallery/" class="transition-all duration-100 no-underline text-gray-7 font-500 hover:tracking-wide">Gallery</NuxtLink>
     </nav>
-    <div class="flex items-center justify-center gap-8px">
+    <div class="flex items-center justify-center gap-10px">
       <a href="https://github.com/axuata"><div class="i-logos:github-icon size-20px" /></a>
       <a href="https://x.com/axuata_x"><div class="i-logos:x size-20px" /></a>
     </div>
